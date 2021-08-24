8651788dae149e2bddd80079428e8e08336ae303
Kevin Schoen <kevinschoen05@gmail.com>
2021-08-23 22:12:00 -0400

    Trips list by reservoir
    Filtering logic to separate the trips list by reservoir based on route selected from landing page

--- src/components/pages/TripsList.vue
+++ src/components/pages/TripsList.vue
@@ -1,10 +1,19 @@
 <template>
   <ul>
+    <div v-if="this.reservoir === 'croton'">
       <trips-item
         v-for="trips in crotonTrips"
         :key="trips.id"
         :tripDate="trips.timestamp"
       ></trips-item>
+    </div>
+    <div v-if="this.reservoir === 'muscoot'">
+      <trips-item
+        v-for="trips in muscootTrips"
+        :key="trips.id"
+        :tripDate="trips.timestamp"
+      ></trips-item>
+    </div>
   </ul>
 </template>
 
@@ -12,6 +21,7 @@
 import TripsItem from "../../trips/TripsItem.vue";
 export default {
   components: { TripsItem },
+  props: ["reservoir"],
   computed: {
     crotonWeight() {
       return this.$store.getters["crotonWeight"];
